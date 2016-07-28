062dc2c79cbde944c55bfd76a19004a15371ccc1
stephen.wille <stephen.wille@gmail.com>
2016-07-28 06:27:35 -0700

    clean up

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,5 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
 import {OfferComponent} from './offer/offer.component';
 
-bootstrap(AppComponent);
 bootstrap(OfferComponent);
 
